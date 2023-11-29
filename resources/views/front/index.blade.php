06a40babcc489409e4df028516e7cee01b84c493
Mahi samia <mahisamia13@gmail.com>
2023-11-29 11:48:00 +0100

    fixing slick slider issue with additional div

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -31,20 +31,20 @@
 
                     </div> <!-- end s-hero__slide -->
                 @endforeach
+            </div>
 
-
-                <div class="nav-arrows s-hero__nav-arrows">
-                    <button class="s-hero__arrow-prev">
-                        <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg" width="15" height="15">
-                            <path d="M1.5 7.5l4-4m-4 4l4 4m-4-4H14" stroke="currentColor"></path>
-                        </svg>
-                    </button>
-                    <button class="s-hero__arrow-next">
-                        <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg" width="15" height="15">
-                            <path d="M13.5 7.5l-4-4m4 4l-4 4m4-4H1" stroke="currentColor"></path>
-                        </svg>
-                    </button>
-                </div> <!-- end s-hero__arrows -->
+            <div class="nav-arrows s-hero__nav-arrows">
+                <button class="s-hero__arrow-prev">
+                    <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg" width="15" height="15">
+                        <path d="M1.5 7.5l4-4m-4 4l4 4m-4-4H14" stroke="currentColor"></path>
+                    </svg>
+                </button>
+                <button class="s-hero__arrow-next">
+                    <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg" width="15" height="15">
+                        <path d="M13.5 7.5l-4-4m4 4l-4 4m4-4H1" stroke="currentColor"></path>
+                    </svg>
+                </button>
+            </div> <!-- end s-hero__arrows -->
 
         </section> <!-- end s-hero -->
 
